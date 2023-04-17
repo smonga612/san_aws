95048b4f8ab768f195f369e3709f59f3384077a2
Sandeep <smongaphd6@gmail.com>
2023-04-17 22:36:18 +0530

    sixthcommit

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from flask import Flask,request,render_template,jsonify
-#from src.piplines.predicition_pipeline import CustomData,PredictPipeline
+from src.pipelines.prediction_pipeline     import CustomData,PredictPipeline
 #from src.piplines.training_pipeline
 
 application=Flask(__name__)
@@ -14,7 +14,22 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('form.html')
     else:
-        result=1
+        data=CustomData(
+            carat=float(request.form.get('carat')),
+            depth=float(request.form.get('depth')),
+            table=float(request.form.get('table')),
+            x=float(request.form.get('x')),
+            y=float(request.form.get('y')),
+            z=float(request.form.get('z')),
+            cut=request.form.get('cut'),
+            color=request.form.get('color'),
+            clarity=request.form.get('clarity')
+
+            )
+    final_df=data.get_data_as_dataframe()
+    predict_pipeline=PredictPipeline()
+    pred=predict_pipeline.predict(final_df)
+    result=round(pred[0],2)
 
     return render_template('result.html',final_result=result)
 
