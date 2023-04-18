8ca42fba6c33b7e271b20bdc1fa386b2754b7753
Sandeep <smongaphd6@gmail.com>
2023-04-18 22:43:17 +0530

    40commit

--- application.py
+++ application.py
@@ -52,11 +52,11 @@ def predict_datapoint():
     '''        
     final_df=data.get_data_as_dataframe()
     predict_pipeline=PredictPipeline()
-    pred=predict_pipeline.predict(final_df)
+    #pred=predict_pipeline.predict(final_df)
     #result=round(pred[0],2)
     
     
-    return render_template('result.html',final_result=carat)
+    return render_template('result.html',final_result=final_df)
 
 if __name__ =="__main__":
     application.run(host='0.0.0.0')
