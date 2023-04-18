3f6ba7a040ed0f7cab4f9de1d77452964656d135
Sandeep <smongaphd6@gmail.com>
2023-04-18 22:51:58 +0530

    40commit

--- application.py
+++ application.py
@@ -52,7 +52,7 @@ def predict_datapoint():
     '''        
     final_df=data.get_data_as_dataframe()
     predict_pipeline=PredictPipeline()
-    #pred=predict_pipeline.predict(final_df)
+    pred=predict_pipeline.predict(final_df)
     #result=round(pred[0],2)
     
     
