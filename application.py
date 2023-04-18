cbcc8712507394442639ab5acb177e8761527c96
Sandeep <smongaphd6@gmail.com>
2023-04-18 20:22:29 +0530

    41commit

--- application.py
+++ application.py
@@ -21,7 +21,7 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('form.html')
     else:
-       ''' 
+        
         data=CustomData(
             
             carat=float(request.form.get('carat')),
@@ -35,7 +35,7 @@ def predict_datapoint():
             clarity=request.form.get('clarity')
 
             )
-            
+    '''        
     final_df=data.get_data_as_dataframe()
     predict_pipeline=PredictPipeline()
     pred=predict_pipeline.predict(final_df)
