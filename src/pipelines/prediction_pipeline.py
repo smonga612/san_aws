c582d69ee1b6c01e8702f55c70350217d7c2ef81
Sandeep <smongaphd6@gmail.com>
2023-04-18 23:31:32 +0530

    40commit

--- src/pipelines/prediction_pipeline.py
+++ src/pipelines/prediction_pipeline.py
@@ -17,7 +17,8 @@ class PredictPipeline:
            preprocessor=load_object(preprocessor_path)
            model=load_object(model_path)
 
-           #data_scaled=preprocessor.transform(features)
+           
+           data_scaled=preprocessor.transform(features)
 
            #pred=model.predict(data_scaled)
            return features
